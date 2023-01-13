8a3259b7983cc6c7f3175116a133c8d6b5c112eb
snowrobot <122578551+snowrobot@users.noreply.github.com>
2023-01-12 16:49:47 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 
 
-streamlit.multiselect("Pick some fruits:", list(
 streamlit.dataframe(my_fruit_list)
 
 
